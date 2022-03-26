3bebc7eb8c982f3fb4cf8d8d41e79c5646a3cfd9
Musa Lawrence <musa.lawrence93@gmail.com>
2022-03-27 00:22:09 +0100

    The Completed Version 1.0 of the Online Wallpaper and Editor Admin App.

---
+++ app/src/main/java/com/thenativecitizens/onlinewallpapereditoradmin/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.thenativecitizens.onlinewallpapereditoradmin.util
+
+
+const val keyAddMainDialog = "ADD_MAIN_DIALOG"
+const val keyAddCategoryDialog = "ADD_CATEGORY_DIALOG"
+const val keyAddSubCategoryDialog = "ADD_SUB_CATEGORY_DIALOG"
+const val keyAddImagesDialog = "ADD_IMAGES_DIALOG"
+const val keyDeleteDialog = "DELETE_DIALOG"
+const val keyUploadImageDialog = "UPLOAD_IMAGE_DIALOG"
